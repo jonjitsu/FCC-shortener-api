399b4d692b7a6ab3c7924f1b26fb1a6008bbe6e6
Jonathan Willis <jonathanwillis@linux.com>
2016-01-21 00:01:38 -0500

    Added configuration and base server

---
+++ src/server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+var assert = require('assert'),
+    config = require('configuror')(),
+    express = require('express'),
+    useragent = require('express-useragent'),
+    accepts = require('accepts'),
+    jsonBeautifier = function(req, res, next) {
+        console.log(req.query.pretty);
+        if(req.query.pretty!==undefined) req.app.set('json spaces', 4);
+        next();
+    },
+
+    app = express()
+    .use(jsonBeautifier)
+     .get('/', function(req, res) {
+         res.send('It works!');
+     })
+    .get('/api/heroku', function(req, res) {
+        res.json({
+            params: req.params,
+            query: req.query,
+            headers: req.headers
+        });
+    })
+     .use(function(req, res) {
+         res.sendStatus(404);
+     }),
+     server = app.listen(config.app.port, function() {
+         let port = server.address().port;
+         console.log('Up an runnin');
+     });
