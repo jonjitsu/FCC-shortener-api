5da1db50dca9d3bb7937c865284c33c52f510609
Jonathan Willis <jonathanwillis@linux.com>
2016-01-21 01:54:05 -0500

    Setup basic mongodb/mongoose driver

--- build/server.js
+++ build/server.js
@@ -5,6 +5,16 @@ var assert = require('assert'),
     express = require('express'),
     useragent = require('express-useragent'),
     accepts = require('accepts'),
+    withDb = function (fn) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect(config.db.uri, function (err, db) {
+        assert.equal(null, err);
+        console.log('Connected to Mongo server');
+
+        fn(db);
+    });
+},
     jsonBeautifier = function (req, res, next) {
     console.log(req.query.pretty);
     if (req.query.pretty !== undefined) req.app.set('json spaces', 4);
@@ -13,11 +23,18 @@ var assert = require('assert'),
     app = express().use(jsonBeautifier).get('/', function (req, res) {
     res.send('It works!');
 }).get('/info/heroku', function (req, res) {
-    res.json({
-        params: req.params,
-        query: req.query,
-        headers: req.headers,
-        config: config
+    withDb(function (db) {
+        db.collection('urls').find().toArray(function (err, data) {
+            if (err) return;
+            res.json({
+                params: req.params,
+                query: req.query,
+                headers: req.headers,
+                data: data
+                // config: config
+            });
+            db.close();
+        });
     });
 }).use(function (req, res) {
     res.sendStatus(404);
--- src/server.js
+++ src/server.js
@@ -6,6 +6,20 @@ var assert = require('assert'),
     express = require('express'),
     useragent = require('express-useragent'),
     accepts = require('accepts'),
+
+
+    withDb = function(fn) {
+        var MongoClient = require('mongodb').MongoClient;
+
+
+        MongoClient.connect(config.db.uri, function(err, db) {
+            assert.equal(null, err);
+            console.log('Connected to Mongo server');
+
+            fn(db);
+        });
+    },
+
     jsonBeautifier = function(req, res, next) {
         console.log(req.query.pretty);
         if(req.query.pretty!==undefined) req.app.set('json spaces', 4);
@@ -18,11 +32,18 @@ var assert = require('assert'),
          res.send('It works!');
      })
     .get('/info/heroku', function(req, res) {
-        res.json({
-            params: req.params,
-            query: req.query,
-            headers: req.headers,
-            config: config
+        withDb(function(db) {
+            db.collection('urls').find().toArray(function(err, data) {
+                if(err) return;
+                res.json({
+                    params: req.params,
+                    query: req.query,
+                    headers: req.headers,
+                    data:data
+                    // config: config
+                });
+                db.close();
+            });
         });
     })
      .use(function(req, res) {
